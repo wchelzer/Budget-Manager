54107fb4795ccb7a1f9c8df4ad19021c18514567
wchelzer <whelzer29@gmail.com>
2023-12-02 19:11:58 -0500

    Created register_frame function

--- Budget Manager/main.py
+++ Budget Manager/main.py
@@ -1,6 +1,6 @@
 # Author: Wyatt Helzer
 # Creation Date: 12/1/2023
-# Date Last Updated: 12/1/2023
+# Date Last Updated: 12/2/2023
 # Function: ...
 
 #---------------------------------------------------------------------------------------------
@@ -13,44 +13,6 @@ from tkinter import Toplevel
 from tkinter import filedialog
 
 
-
-class BudgetGUI:
-
-    def __init__(self):
-        self.main = tkinter.Tk()
-        self.main_frame = tkinter.Frame(self.main)
-        
-        # main screen buttons
-        self.login_button = tkinter.Button(self.main_frame, text="LOGIN", command = self.login_frame)
-        self.register_button = tkinter.Button(self.main_frame, text="REGISTER", command = self.register)
-        self.no_register_button = tkinter.Button(self.main_frame, text="CONTINUE WITHOUT\n REGISTERING", command = self.register) # change the command for this button
-        
-        self.login_button.pack(side="top")
-        self.register_button.pack(side="bottom")
-        self.no_register_button.pack(side="bottom")
-
-        self.main_frame.pack()
-
-
-
-        tkinter.mainloop()
-        
-        
-    def login_frame(self):
-        # print("Login")
-        self.login_screen = Toplevel(self.main)
-        self.login_screen.title("Login Page")
-
-        
-
-
-    def register(self):
-        print("Register")
-
-
-
-
-
 if __name__ == "__main__":
     
     # Creates database connection
@@ -59,4 +21,84 @@ if __name__ == "__main__":
     user = "root",
     password = "Mr_Kaboom1939")
     
-    BudgetGUI()
+    # Creates main menu
+    main = tkinter.Tk()
+    main.title("Budget Manager")
+    
+
+
+
+    def register_frame():
+        global register_username
+        global register_password
+        global register_password_verification
+        
+        register_screen = Toplevel(main)
+        register_screen.title("Register Page")
+
+        register_username = tkinter.StringVar()
+        register_password = tkinter.StringVar()
+        register_password_verification = tkinter.StringVar()
+
+        # labels
+        register_info_label = tkinter.Label(register_screen, text="Enter new login information")
+        register_username_label = tkinter.Label(register_screen, text="Username")
+        register_password_label = tkinter.Label(register_screen, text="Password")
+        register_password_verification_label = tkinter.Label(register_screen, text="Confirm your password")
+
+        # entries
+        register_username_entry = tkinter.Entry(register_screen, textvariable=register_username)
+        register_password_entry = tkinter.Entry(register_screen, textvariable=register_password, show="*")
+        register_password_verification_entry = tkinter.Entry(register_screen, textvariable=register_password_verification, show="*")
+
+        # button
+        register_button = tkinter.Button(register_screen, text="REGISTER", command=filler)
+
+        # grid
+        register_info_label.grid(row=0, column=0, columnspan=2)
+        register_username_label.grid(row=1, column=0)
+        register_username_entry.grid(row=1, column=1)
+        register_password_label.grid(row=2, column=0)
+        register_password_entry.grid(row=2, column=1)
+        register_password_verification_label.grid(row=3, column=0)
+        register_password_verification_entry.grid(row=3, column=1)
+        register_button.grid(row=4, column=1)
+
+
+
+
+
+    def login_frame():
+        # print("Login")
+        login_screen = Toplevel(main)
+        login_screen.title("Login Page")
+
+
+    def filler():
+        print("You clicked me!")
+
+
+
+    # main screen info
+    username = tkinter.StringVar()
+    password = tkinter.StringVar()
+    
+    login_button = tkinter.Button(main, text="LOGIN", command = login_frame)
+    register_button = tkinter.Button(main, text="REGISTER", command = register_frame)
+    continue_button = tkinter.Button(main, text="CONTINUE WITHOUT\n REGISTERING", command = register_frame) # change the command for this button
+    username_label = tkinter.Label(main, text="Username")
+    password_label = tkinter.Label(main, text="Password")
+    username_entry = tkinter.Entry(main, textvariable=username)
+    password_entry = tkinter.Entry(main, textvariable=password, show="*")
+    
+    username_label.grid(row=0, column=0)
+    username_entry.grid(row=0, column=1)
+    password_label.grid(row=1, column=0)
+    password_entry.grid(row=1, column=1)
+    login_button.grid(row=0, column=2, rowspan=2)
+    register_button.grid(row=2, column=0)
+    continue_button.grid(row=2, column=1)
+    
+    
+    tkinter.mainloop()
+    
